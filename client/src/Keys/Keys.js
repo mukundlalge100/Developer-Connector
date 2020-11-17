2658c68b4c9980cf9e58bb2aee0c44c23ad5bbab
Mukund Lalge <mukundlalge100@gmail.com>
2020-11-17 15:03:44 +0530

    keys remove from github

--- client/src/Keys/Keys.js
+++ client/src/Keys/Keys.js
@@ -1,4 +1,4 @@
 export const Github = {
-  clientId: "6a7c3b3e3d009560643e",
-  clientSecret: "2c59ec0a588f332948cc94d9e27c0444e41c37af"
+  clientId: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET
 };
